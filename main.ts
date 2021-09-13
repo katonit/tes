7d6460f25b91e7e527c51f2c3e806e9e0fa1781f
katonit <46913937+katonit@users.noreply.github.com>
2021-09-13 17:00:04 +0900

    更新 pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,7 @@
-basic.forever(function on_forever() {
-if (
-input.buttonIsPressed(Button.A)
-basic.showIcon(IconNames.EigthNote)
-} else {
-basic.clearScreen()
-}
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.B)) {
+        basic.showIcon(IconNames.EigthNote)
+    } else {
+        basic.clearScreen()
+    }
 })
