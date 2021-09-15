db349b18056475ebeba6f612c7bf292fd5a5359b
katonit <46913937+katonit@users.noreply.github.com>
2021-09-15 10:00:59 +0900

    ファイルの説明

--- main.ts
+++ main.ts
@@ -2,8 +2,8 @@
 basic.forever(function on_forever() {
 if (
 input.buttonIsPressed(Button.A)
-basic.showIcon(IconNames.EigthNote)
 ) {
+basic.showIcon(IconNames.EigthNote)
 } else {
 basic.clearScreen()
 }
